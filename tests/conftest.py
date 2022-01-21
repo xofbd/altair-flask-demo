5c73bc07473f1552e363c505585b0b7060a1bdf6
xofbd <dfox09@gmail.com>
2022-01-21 10:47:02 -0500

    Add unit tests for functions in plot module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 
 from app.app import app
@@ -12,3 +13,21 @@ def test_client():
 @pytest.fixture
 def form_data():
     return {'depth_min': 500, 'grad_min': 0.05}
+
+
+@pytest.fixture
+def well_coords():
+    return [
+        (35.8883487, -106.5795905, 1464.9, 0.16519898969212915),
+        (35.8760489, -106.5867907, 2525.0, 0.12792079207920792),
+    ]
+
+
+@pytest.fixture
+def well_coords_df():
+    return pd.DataFrame({
+        'latitude': [35.8883487, 35.8760489],
+        'longitude': [-106.5795905, -106.5867907],
+        'depth': [1464.9, 2525.0],
+        'gradient': [0.16519898969212915, 0.12792079207920792],
+    })
