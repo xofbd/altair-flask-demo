6234c3325b4b5fd31c29dd958f7643d2bbf5ff59
xofbd <dfox09@gmail.com>
2022-02-06 16:33:17 -0500

    Properly manage secret key used for form validation
    - set secret key to dummy value for non production configuration
    - create bin/set_secret_key
    - update README with instructions for setting secret key

--- config.py
+++ config.py
@@ -2,18 +2,21 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.urandom(32)
+    pass
 
 
 class ConfigProd(Config):
     FLASK_ENV = 'production'
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 class ConfigDev(Config):
     FLASK_ENV = 'development'
+    SECRET_KEY = 'secret key'
 
 
 class ConfigTesting(Config):
     FLASK_ENV = 'testing'
+    SECRET_KEY = 'secret key'
     TESTING = True
     WTF_CSRF_ENABLED = False
